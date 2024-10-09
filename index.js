950dfa144143f6d80da10be09e447c4f06acea60
Jordid13 <jordidiaz2020@gmail.com>
2024-10-09 10:10:53 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const sumOfThreeOrFive = () => {
     let result = 0
     while (i < 1000) {
         if (i % 3 === 0 || i % 5 === 0) {
-            result = result + i
+            result += i
         }
         i++
     }
